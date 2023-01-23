a6312b2d185a3b29062a49495d47cfb73b0e5bb8
GrzegorzManiak <git@grzegorz.ie>
2023-01-23 21:55:46 +0000

    Init Django Project and Webpack Env

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+
+module.exports = {
+    entry: {
+        // consistent_hashing: './scripts/consistent_hashing/index.ts',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules|\.d\.ts$/
+            },
+            {
+                test: /\.d\.ts$/,
+                loader: 'ignore-loader'
+            },         
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }  
+        ],
+    },
+    resolve: {
+        extensions: [ '.ts', '.js' ],
+    },
+    output: {
+        filename: '[name]_bin.js',
+        path: path.resolve(__dirname, './master/StreamStage/static/js'),
+    },
+};
