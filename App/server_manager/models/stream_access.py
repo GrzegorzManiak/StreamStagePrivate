847b28e3825202375bae731f1aa0b7e06c114fc7
GrzegorzManiak <git@grzegorz.ie>
2023-01-26 22:48:17 +0000

    Init branch, Added steam_access functionality #39

---
+++ App/server_manager/models/stream_access.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.core.validators import MinLengthValidator
+import uuid
+
+class StreamAccess(models.Model):
+
+    id = models.UUIDField(
+        primary_key=True, 
+        default=uuid.uuid4, 
+        editable=False
+    )
+
+    user = models.ForeignKey(
+        'Member',
+        on_delete=models.CASCADE,
+        related_name='stream_access'
+    )
+
+    key = models.CharField(
+        validators=[
+            MinLengthValidator(32)
+        ],
+    )
+
+    # ========= Temporary ========= #
+    # TODO: Remove this field after 
+    #       the stream is implemented
+    # ============================= #
+    stream = models.TextField()
+
+
+    def __str__(self):
+        return f'{self.user} - {self.stream}'
