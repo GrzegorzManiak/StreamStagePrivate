14de89a81c9f31fecd401fe76f7fc44d22f62a50
GrzegorzManiak <git@grzegorz.ie>
2023-01-30 13:19:05 +0000

    Added functionality #48

--- App/server_manager/library/__init__.py
+++ App/server_manager/library/__init__.py
@@ -1,14 +1,2 @@
-from .stream_access import (
-    generate_key,
-    invalidate_key,
-    key_exists,
-    invalidate_key_by_id,
-    invalidate_key_by_member_id,
-    key_exists_by_id,
-    key_exists_by_member_id,
-
-    get_key_by_member_id,
-    get_keys_by_stream_id,
-    get_key,
-    get_key_by_id,
-)
+from .stream_access import *
+from .common import *
--- App/server_manager/models/server.py
+++ App/server_manager/models/server.py
@@ -1,5 +1,10 @@
 from django.db import models
-from django.core.validators import MinLengthValidator
+from django.core.validators import (
+    MinLengthValidator, 
+    ip_address_validators
+)
+from library import ServerMode
+
 import uuid
 import secrets
 
@@ -58,3 +63,51 @@ class Server(models.Model):
     live = models.BooleanField(
         default=False,
     )
+
+    def __str__(self):
+        return self.name
+
+
+    # ========= Functions ========= #
+    def set_mode(self, mode: ServerMode) -> None or ValueError:
+        mode = mode.to_string()
+
+        if mode is not None: self.mode = mode
+        else: raise ValueError('Invalid server mode')
+
+    def get_mode(self) -> ServerMode or ValueError:
+        mode = ServerMode.from_string(self.mode)
+
+        if mode is not None: return mode
+        else: raise ValueError('Invalid server mode')
+
+    def announce(self, ip, port, hb_id) -> None:
+        self.live = True
+        
+        # Validate the IP and port
+        try: ip_address_validators('ipv4', ip)
+        except: raise ValueError('Invalid IP address')
+
+        # Validate the port
+        if port < 0 or port > 65535:
+            raise ValueError('Invalid port')
+
+        # TODO: Here we would spawn a thread to
+        # monitor the heartbeat of the server
+        # but we haven't implemented the 
+        # heartbeat system yet
+
+        self.ip = ip
+        self.port = port
+
+        self.save()
+
+
+    def save(self, *args, **kwargs):
+        # Validate that if live is True, then the IP and port are not None
+        if self.live:
+            if self.ip is None or self.port is None:
+                raise ValueError('Server is live, but IP or port is None')
+
+        super().save(*args, **kwargs)
+
