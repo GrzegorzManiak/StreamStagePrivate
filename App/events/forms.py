8f3b4c8fee5d708ee27e0833c83d35e9766aacad
kiverisb <ben@syclonestudios.com>
2023-02-19 08:52:33 +0000

    Exclude event_id from event update form

--- App/events/forms.py
+++ App/events/forms.py
@@ -26,6 +26,7 @@ class EventUpdateForm(forms.ModelForm):
         model = Event
     
         exclude = [
+            "event_id",
             "streamer",
             "approved",
             "contributors",
