340cd0be5a1e2d6f12fe5f8445cad784e8a9de96
kiverisb <ben@syclonestudios.com>
2023-02-19 11:51:58 +0000

    Fix delete event view, and add check for non existent event for event_view

--- App/events/views.py
+++ App/events/views.py
@@ -33,6 +33,10 @@ def generate_event_id():
 # Viewing an individual Event
 def event_view(request, event_id):
     event = Event.objects.filter(event_id=event_id).first()
+
+    if event == None:
+        return redirect('all_events')
+
     reviews = EventReview.objects.filter(event=event).all()
     primary_media_idx = event.primary_media_idx
     
@@ -80,31 +84,37 @@ def event_update(request, event_id):
 
     if not request.user.is_authenticated or not request.user.is_streamer:
         return redirect('all_events')
-    if event == None: # if event id in URL is invalid
+    if event == None: # if event id in URL is invalid, redirect
         return redirect('all_events')
     
-    if request.POST:
-        form = EventUpdateForm(instance=event, data=request.POST)
+    form = EventUpdateForm(instance=event, data=request.POST)
+    if form.is_valid():
         form.save()
-
         return redirect('event_view', event.event_id)
     else:
         form = EventUpdateForm(instance=event)
     
-    context['form']=  form
+    context['form'] =  form
     return render(request, "event_update.html", context)
         
-def event_delete(request):
+def event_delete(request, event_id):
     context = {}
-
-    form = EventDeleteForm(request.POST or None)
+    event = Event.objects.get(event_id=event_id)
+    
     if not request.user.is_authenticated or not request.user.is_streamer:
         return redirect('all_events')
-    if form.is_valid():
-        form.save()
+    # if event id in URL is invalid or user doesn't own this event, redirect
+    if event == None or not request.user == event.streamer:
+        return redirect('all_events')
+    
+    form = EventDeleteForm(instance=event)
+    if request.POST:
+        event.delete()
         return redirect('member_profile')
-
-    context['form']= form
+    else:
+        form = EventDeleteForm(instance = event)
+    
+    context['form'] = form
     return render(request, "event_delete.html", context)
 
 
