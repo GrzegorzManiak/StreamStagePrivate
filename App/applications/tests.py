e802592786112870c792db49433b7218498c4535
kiveris <ben@kiver.is>
2023-03-02 11:24:36 +0000

    Write tests for remaining application processing functions

--- App/applications/tests.py
+++ App/applications/tests.py
@@ -1,9 +1,12 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
+
 from applications.models import StreamerApplication, EventApplication, BroadcasterApplication
 from accounts.models import Member, Broadcaster
 from events.models import Event, Category
+
+from applications.models import STATUS_REJECTED, STATUS_APPROVED, STATUS_WAITING
 
 from applications.processing import (
     submit_broadcaster_application,
@@ -28,16 +31,22 @@ class ApplicationTests(TestCase):
     def setUp(self):
         # Create a test Member that is a streamer
         self.test_streamer = Member(
-            username = 'Gediminas_Kreg',
-            cased_username = "ged1min4s",
+            username = 'ged_kreg',
             email = 'gedukas@garblox.com',
             country = 'LT',
             is_streamer = True
         )
         self.test_streamer.save()
 
+        self.test_admin = Member(
+            is_staff = True,
+            username = "Admin01",
+            email = "admin@streamstage.co",
+            country = "IE"
+        )
+        self.test_admin.save()
 
-        # Create a test Category (for use in the event creation test)
+        # Create a test Category (for use in the event application creation test)
         self.test_category = Category(
             name = "Test Category",
             description = "This is an excellent category",
@@ -50,16 +59,18 @@ class ApplicationTests(TestCase):
         test_submission_statement = "I would like to become a streamer!"
         test_applicant = self.test_streamer
 
-        test_data = {
+        test_app_data = {
             "submission_statement": test_submission_statement
         }
 
-        submit_streamer_application(test_applicant, test_data)
+        submit_streamer_application(test_applicant, test_app_data)
 
         application = StreamerApplication.objects.filter(applicant=self.test_streamer).first()
 
+        # test that application was created and has correct data
         self.assertTrue(application is not None)
         self.assertEqual(application.submission_statement, test_submission_statement)
+        self.assertEqual(application.applicant, test_applicant)
     
     def test_submit_broadcaster_application(self):
         test_submission_statement = "I would like to become a broadcaster!"
@@ -69,19 +80,21 @@ class ApplicationTests(TestCase):
 
         test_applicant = self.test_streamer
 
-        test_data = {
+        test_app_data = {
             "submission_statement": test_submission_statement,
             "handle": test_handle,
             "name": test_name,
             "biography": test_biography
         }
 
-        submit_broadcaster_application(test_applicant, test_data)
+        submit_broadcaster_application(test_applicant, test_app_data)
 
+        # query for application
         application = BroadcasterApplication.objects.filter(applicant=self.test_streamer).first()
 
         self.assertTrue(application is not None)
         self.assertEqual(application.submission_statement, test_submission_statement)
+        self.assertEqual(application.broadcaster.handle, test_handle)
 
     def test_submit_event_application(self):
         test_broadcaster = self.broadcaster_or_new()
@@ -104,8 +117,59 @@ class ApplicationTests(TestCase):
         self.assertTrue(application is not None)
         self.assertEqual(application.event.title, test_title)
 
+
+    def test_approve_streamer_application(self):
+        test_streamer_app = self.streamer_app_or_new()
+
+        approve_streamer_application(test_streamer_app, self.test_admin)
+        
+        self.assertTrue(test_streamer_app.status, STATUS_APPROVED)
+        self.assertTrue(test_streamer_app.processed_by, self.test_admin)
+    
+    def test_reject_streamer_application(self):
+        test_streamer_app = self.streamer_app_or_new()
+
+        reject_streamer_application(test_streamer_app, self.test_admin)
+        
+        self.assertTrue(test_streamer_app.status, STATUS_REJECTED)
+        self.assertTrue(test_streamer_app.processed_by, self.test_admin)
+
+
+    def test_approve_event_application(self):
+        test_event_app = self.event_app_or_new()
+
+        approve_event_application(test_event_app, self.test_admin)
+        
+        self.assertTrue(test_event_app.status, STATUS_APPROVED)
+        self.assertTrue(test_event_app.processed_by, self.test_admin)
+    
+    def test_reject_event_application(self):
+        test_event_app = self.event_app_or_new()
+
+        reject_event_application(test_event_app, self.test_admin)
+        
+        self.assertTrue(test_event_app.status, STATUS_REJECTED)
+        self.assertTrue(test_event_app.processed_by, self.test_admin)
+
+    def test_approve_broadcaster_application(self):
+        test_broadcaster_app = self.broadcaster_app_or_new()
+
+        approve_broadcaster_application(test_broadcaster_app, self.test_admin)
+        
+        self.assertTrue(test_broadcaster_app.status, STATUS_APPROVED)
+        self.assertTrue(test_broadcaster_app.processed_by, self.test_admin)
+    
+    def test_reject_broadcaster_application(self):
+        test_broadcaster_app = self.broadcaster_app_or_new()
+
+        reject_broadcaster_application(test_broadcaster_app, self.test_admin)
+        
+        self.assertTrue(test_broadcaster_app.status, STATUS_REJECTED)
+        self.assertTrue(test_broadcaster_app.processed_by, self.test_admin)
+
+
     def broadcaster_or_new(self):
-        broadcaster = Broadcaster.objects.filter(handle="TestBroadcaster").first()
+        broadcaster = Broadcaster.objects.first()
 
         if broadcaster is None:
             broadcaster = Broadcaster(
@@ -119,5 +183,50 @@ class ApplicationTests(TestCase):
     
         return broadcaster
 
+    def streamer_app_or_new(self):
+        streamer_app = StreamerApplication.objects.first()
 
+        if streamer_app is None:
+            streamer_app = StreamerApplication(
+                applicant = self.test_streamer,
+                submission_statement = "I'd like to become a streamer."
+            )
 
+            streamer_app.save()
+        
+        return streamer_app
+
+    def event_app_or_new(self):
+        event_app = EventApplication.objects.first()
+
+        if event_app is None:
+            test_broadcaster = self.broadcaster_or_new()
+
+            test_event = Event(
+                title = "New Event 02",
+                description = "A cool event for tests!",
+                broadcaster = test_broadcaster
+            )
+            test_event.save()
+
+            event_app = EventApplication(
+                applicant = self.test_streamer,
+                event = test_event
+            )
+            event_app.save()
+        
+        return event_app
+    
+    def broadcaster_app_or_new(self):
+        broadcaster_app = BroadcasterApplication.objects.first()
+
+        if broadcaster_app is None:
+            test_broadcaster = self.broadcaster_or_new()
+
+            broadcaster_app = BroadcasterApplication(
+                applicant = self.test_streamer,
+                broadcaster = test_broadcaster
+            )
+            broadcaster_app.save()
+        
+        return broadcaster_app
