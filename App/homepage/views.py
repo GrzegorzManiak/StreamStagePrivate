1cb7452e91ad943543f6a969d1d32940af93fded
Grzegorz Maniak <git@grzegorz.ie>
2023-03-08 18:16:15 +0000

    Added thumbnails

--- App/homepage/views.py
+++ App/homepage/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from rest_framework.decorators import api_view
+from django.conf import settings
 
-# Create your views here.
+"""
+    Main homepage view, first page a user sees when they visit the site
+"""
+@api_view(['GET'])
+def index(request):
+
+    context = {
+        'is_admin': request.user.is_superuser,
+        'base_url': settings.DOMAIN_NAME,
+    }
+
+    return render(
+        request, 
+        'main.html', 
+        context
+    )
