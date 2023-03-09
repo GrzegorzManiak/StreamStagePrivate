ad5660146931af79aa9a9f66e561d91543b46114
kiveris <ben@kiver.is>
2023-03-09 12:09:31 +0000

    Getting started w/ order item models

--- App/store/models.py
+++ App/store/models.py
@@ -6,6 +6,7 @@ class Ticket(models.Model):
     ticket_id = models.CharField(primary_key=True, unique=True, max_length=20)
     event = models.ForeignKey(Event, null=True, on_delete=models.DO_NOTHING) # if event is deleted - keep ticket
     showing = models.ForeignKey(EventShowing, null=True, on_delete=models.DO_NOTHING) # nullable - can buy ticket without specific showing?
+    
     ticket_price = models.FloatField("Price")
     ticket_type = models.CharField("Ticket Type", max_length=20) # 'Online' / 'Special' - yadda yadda
 
