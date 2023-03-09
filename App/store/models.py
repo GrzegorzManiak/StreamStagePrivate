dcab1b4678099f677dffaefb3e147ebc3ac2dc7a
kiveris <ben@kiver.is>
2023-03-09 11:52:21 +0000

    ticket model

--- App/store/models.py
+++ App/store/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from accounts.models import Member
+from events.models import Event, EventShowing
 
-# Create your models here.
+class Ticket(models.Model):
+    ticket_id = models.CharField(primary_key=True, unique=True, max_length=20)
+    event = models.ForeignKey(Event)
+    showing = models.ForeignKey(EventShowing, null=True) # nullable - can buy ticket without specific showing?
+    ticket_price = models.FloatField("Price")
+    ticket_type = models.CharField("Ticket Type", max_length=20) # 'Online' / 'Special' - yadda yadda
+
