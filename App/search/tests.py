354d311f4f0e588cfdd9c8d4c1123ab66a2e812d
kiverisb <ben@syclonestudios.com>
2023-03-24 21:55:54 +0000

    Implemented nine search app tests

--- App/search/tests.py
+++ App/search/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase, RequestFactory
+from django.test import Client, TestCase, RequestFactory
 from .views import SearchResultsListView
 from events.models import Category, Event, EventShowing
 from accounts.models import Member, Broadcaster
@@ -28,8 +28,8 @@ class EventTests(TestCase):
         )
 
         # Making a variable for calling many-to-many set in later tests
-        comedy_category = Category.objects.all().filter(name='Comedy').first()
-        theatre_category = Category.objects.all().filter(name='Theatre').first()
+        self.comedy_category = Category.objects.all().filter(name='Comedy').first()
+        self.theatre_category = Category.objects.all().filter(name='Theatre').first()
 
         # Create Test Broadcaster
         self.broadcaster = Broadcaster.objects.create(
@@ -49,7 +49,7 @@ class EventTests(TestCase):
             broadcaster = self.broadcaster, 
             approved = True
         )
-        self.event1.categories.add(comedy_category)
+        self.event1.categories.add(self.comedy_category)
                 
         # Create Test Event 2
         self.event2 = Event.objects.create(
@@ -59,7 +59,7 @@ class EventTests(TestCase):
             broadcaster = self.broadcaster, 
             approved = True
         )
-        self.event2.categories.add(theatre_category)
+        self.event2.categories.add(self.theatre_category)
                 
         # Create Test Event 3
         self.event3 = Event.objects.create(
@@ -69,20 +69,162 @@ class EventTests(TestCase):
             broadcaster = self.broadcaster, 
             approved = True
         )
-        self.event3.categories.add(comedy_category, theatre_category)
+        self.event3.categories.add(self.comedy_category, self.theatre_category)
+
+
+        self.events = [ self.event1, self.event2, self.event3 ]
+
+        self.client = Client()
 
 
     # ********************
     # *** Search Tests ***
     # ********************
 
-    def test_get_queryset_displays_search_results(self):
-        request = RequestFactory().get('/search')
-        view = SearchResultsListView()
-        qs = view.get_queryset
-        self.assertQuerysetEqual(qs, results())
+    # query test category, description, title,
+    # query broadcaster handle
 
-        self.response = self.client.get(self.event.get_absolute_url())
-        self.assertEqual(self.response.status_code, 200)
+    def test_search_query_string_empty(self):
+        response = self.client.get('/search/')
+
+        self.assertEqual(response.status_code, 200)
         # Testing if correct template used
-        self.assertTemplateUsed(self.response, 'search.html') 
+        self.assertTemplateUsed(response, 'search.html') 
+
+        # Testing if query set returns all events
+        self.assertListEqual(list(response.context['events_list']), self.events)
+
+    def test_search_category_comedy(self):
+        comedy_category = self.comedy_category
+
+        comedy_events = [
+            self.event1,
+            self.event3
+        ]
+
+        response = self.client.get('/search/', QUERY_STRING = ('cat=' + comedy_category.name))
+
+        self.assertEqual(response.status_code, 200)
+        # Testing if correct template used
+        self.assertTemplateUsed(response, 'search.html') 
+
+        # Testing if query set returns all events
+        self.assertListEqual(list(response.context['events_list']), comedy_events)
+
+    def test_search_category_theatre(self):
+        theatre_category = self.theatre_category
+
+        theatre_events = [
+            self.event2,
+            self.event3
+        ]
+
+        response = self.client.get('/search/', QUERY_STRING = ('cat=' + theatre_category.name))
+
+        self.assertEqual(response.status_code, 200)
+        # Testing if correct template used
+        self.assertTemplateUsed(response, 'search.html') 
+
+        # Testing if query set returns all events
+        self.assertListEqual(list(response.context['events_list']), theatre_events)
+
+    def test_search_query_string_partial_title(self):
+        query = "3"
+
+        match_events = [
+            self.event3
+        ]
+
+        response = self.client.get('/search/', QUERY_STRING = ('q=' + query))
+
+        self.assertEqual(response.status_code, 200)
+        # Testing if correct template used
+        self.assertTemplateUsed(response, 'search.html') 
+
+        # Testing if query set returns all events
+        self.assertListEqual(list(response.context['events_list']), match_events)
+    
+    def test_search_query_string_partial_category(self):
+        query = "com"
+
+        match_events = [
+            self.event1,
+            self.event3
+        ]
+
+        response = self.client.get('/search/', QUERY_STRING = ('q=' + query))
+
+        self.assertEqual(response.status_code, 200)
+        # Testing if correct template used
+        self.assertTemplateUsed(response, 'search.html') 
+
+        # Testing if query set returns all events
+        self.assertListEqual(list(response.context['events_list']), match_events)
+    
+    def test_search_query_string_partial_description(self):
+        query = "comedy event"
+
+        match_events = [
+            self.event1,
+            self.event3
+        ]
+
+        response = self.client.get('/search/', QUERY_STRING = ('q=' + query))
+
+        self.assertEqual(response.status_code, 200)
+        # Testing if correct template used
+        self.assertTemplateUsed(response, 'search.html') 
+
+        # Testing if query set returns all events
+        self.assertListEqual(list(response.context['events_list']), match_events)
+    
+    def test_search_query_broadcaster_partial_broadcaster_handle(self):
+        broadcaster_query = "roadcast"
+
+        match_events = [
+            self.event1,
+            self.event2,
+            self.event3
+        ]
+
+        response = self.client.get('/search/', QUERY_STRING = ('b=' + broadcaster_query))
+
+        self.assertEqual(response.status_code, 200)
+        # Testing if correct template used
+        self.assertTemplateUsed(response, 'search.html') 
+
+        # Testing if query set returns all events
+        self.assertListEqual(list(response.context['events_list']), match_events)
+    
+    def test_search_query_string_partial_broadcaster_name(self):
+        broadcaster_query = "roadcast"
+
+        match_events = [
+            self.event1,
+            self.event2,
+            self.event3
+        ]
+
+        response = self.client.get('/search/', QUERY_STRING = ('q=' + broadcaster_query))
+
+        self.assertEqual(response.status_code, 200)
+        # Testing if correct template used
+        self.assertTemplateUsed(response, 'search.html') 
+
+        # Testing if query set returns all events
+        self.assertListEqual(list(response.context['events_list']), match_events)    
+
+    def test_search_query_string_no_match(self):
+        query = "zombie"
+
+        match_events = [
+        ]
+
+        response = self.client.get('/search/', QUERY_STRING = ('q=' + query))
+
+        self.assertEqual(response.status_code, 200)
+        # Testing if correct template used
+        self.assertTemplateUsed(response, 'search.html') 
+
+        # Testing if query set returns all events
+        self.assertListEqual(list(response.context['events_list']), match_events)
