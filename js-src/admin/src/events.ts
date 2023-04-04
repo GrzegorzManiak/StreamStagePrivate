0c75cfcb8be2d588977d5391ad2f34972972247a
Grzegorz Maniak <git@grzegorz.ie>
2023-04-04 14:33:19 +0100

    #160 CSS bug

--- js-src/admin/src/events.ts
+++ js-src/admin/src/events.ts
@@ -107,52 +107,54 @@ export function create_event_modal(
 
 
     const template = ` 
-    <div class='w-100 d-flex justify-content-between align-items-center p-2'>
-        <div class='profile-info flex-wrap'>
-            <p class='m-0 text-muted'><span class='bold'> Created:</span> ${date(event.created)}</p>
-            <p class='m-0 text-muted'><span class='bold'> Updated:</span> ${date(event.updated)}</p>
-        </div>
-
-        <div class='profile-info flex-wrap'>
-            <p class='m-0 text-muted'>Over 18's: ${event.over_18 ? '<span class="text-success">Yes</span>' : '<span class="text-danger">No</span>'}</p>
-            <p class='m-0 text-muted'>Approved: ${event.approved ? '<span class="text-success">Yes</span>' : '<span class="text-danger">No</span>'}</p>
-        </div>
-
-        <div class='profile-info flex-wrap'>
-            <p class='m-0 text-muted'><span class='bold'> Showings:</span> ${event.showings.length}</p>
-            <p class='m-0 text-muted'><span class='bold'> Contributors:</span> ${event.contributors.length}</p>
-        </div>
-
-        <div class='profile-actions p-2'>
-            <button class="w-100 h-100 btn btn-primary info loader-btn edit" loader-state="default">   
-                <span> 
-                    <div class="spinner-border" role="status"> 
-                        <span class="visually-hidden">Loading...</span>  
-                    </div>
-                </span>
-
-                <p>Manage</p>
-            </button>
-        </div>
-    </div>
-
-    <div class='w-100 d-flex justify-content-between align-items-center p-2'>
-        <div class='profile-info cat-info'>
-            <div class='m-0 text-muted d-flex justify-content-between w-100'>
-                <h3 class='m-0 w-25'>Broadcaster</h3>
-                <span class='w-25'>${event.broadcaster.handle}</span>
-                <span class='w-50'>ID: ${event.broadcaster.id}</span>
+    <div>
+        <div class='w-100 d-flex justify-content-between align-items-center p-2'>
+            <div class='profile-info flex-wrap'>
+                <p class='m-0 text-muted'><span class='bold'> Created:</span> ${date(event.created)}</p>
+                <p class='m-0 text-muted'><span class='bold'> Updated:</span> ${date(event.updated)}</p>
             </div>
 
-            <div class='m-0 text-muted d-flex justify-content-between w-100'>
-                <h3 class='m-0 w-25'>Title</h3>
-                <span class='w-25'>${event.title}</span>
-                <span class='w-50'>ID: ${event.id}</span>
+            <div class='profile-info flex-wrap'>
+                <p class='m-0 text-muted'>Over 18's: ${event.over_18 ? '<span class="text-success">Yes</span>' : '<span class="text-danger">No</span>'}</p>
+                <p class='m-0 text-muted'>Approved: ${event.approved ? '<span class="text-success">Yes</span>' : '<span class="text-danger">No</span>'}</p>
             </div>
 
-            <h3 class='m-0'>Description: <span class='m-0 text-muted'>${event.description}</span></h3>
+            <div class='profile-info flex-wrap'>
+                <p class='m-0 text-muted'><span class='bold'> Showings:</span> ${event.showings.length}</p>
+                <p class='m-0 text-muted'><span class='bold'> Contributors:</span> ${event.contributors.length}</p>
+            </div>
+
+            <div class='profile-actions p-2'>
+                <button class="w-100 h-100 btn btn-primary info loader-btn edit" loader-state="default">   
+                    <span> 
+                        <div class="spinner-border" role="status"> 
+                            <span class="visually-hidden">Loading...</span>  
+                        </div>
+                    </span>
+
+                    <p>Manage</p>
+                </button>
+            </div>
         </div>
-    </div>
+
+        <div class='w-100 d-flex justify-content-between align-items-center p-2'>
+            <div class='profile-info cat-info'>
+                <div class='m-0 text-muted d-flex justify-content-between w-100'>
+                    <h3 class='m-0 w-25'>Broadcaster</h3>
+                    <span class='w-25'>${event.broadcaster.handle}</span>
+                    <span class='w-50'>ID: ${event.broadcaster.id}</span>
+                </div>
+
+                <div class='m-0 text-muted d-flex justify-content-between w-100'>
+                    <h3 class='m-0 w-25'>Title</h3>
+                    <span class='w-25'>${event.title}</span>
+                    <span class='w-50'>ID: ${event.id}</span>
+                </div>
+
+                <h3 class='m-0'>Description: <span class='m-0 text-muted'>${event.description}</span></h3>
+            </div>
+        </div>
+    <div>
     `;
 
     // -- Create the element
