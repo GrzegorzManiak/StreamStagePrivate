1f23ac946d00edfd222131ee8f852b969a2ef19a
kiverisb <ben@syclonestudios.com>
2023-04-23 18:28:03 +0100

    add missing review view

--- App/events/views.py
+++ App/events/views.py
@@ -150,66 +150,6 @@ def event_delete(request, event_id):
     return render(request, "event_delete.html", context)
 
 
-                                        # ****************
-                                        # *** Showings ***                                        # ***************
-                                        # ****************
-def showing_create(request, event_id):
-    context = {}
-    event = Event.objects.get(event_id=event_id)
-
-    form = ShowingCreateForm(request.POST or None)
-    if not request.user.is_authenticated or not request.user.is_streamer:
-        return redirect('event_view', event_id)
-    if form.is_valid():
-        form = form.save(commit=False)
-        form.streamer = request.user
-        form.event = event
-        form.save()
-
-        return redirect('event_view', event_id)
-
-    context['form']= form
-    return render(request, "showings/showing_new.html", context)
-
-def showing_update(request, event_id, showing_id):
-    context = {}
-    showing = EventShowing.objects.filter(showing_id=showing_id).first()
-
-    if not request.user.is_authenticated or not request.user.is_streamer:
-        return redirect('event_view', event_id)
-    
-    form = ShowingUpdateForm(instance=showing, data=request.POST)
-    if form.is_valid():
-        form.save()
-        return redirect('event_view', event_id)
-    else:
-        form = ShowingUpdateForm(instance=showing)
-    
-    context['form'] =  form
-    return render(request, "showings/showing_update.html", context)
-
-
-def showing_delete(request, event_id, showing_id):
-    showing = EventShowing.objects.filter(showing_id=showing_id).first()
-
-    form = ShowingDeleteForm(instance=showing)
-    if not request.user.is_authenticated or not request.user.is_streamer:
-        return redirect('event_view', event_id)
-    if showing == None:
-        return redirect('event_view', event_id)
-    if request.POST:
-        showing.delete()
-        return redirect('event_view', event_id)
-    else:
-        form = ShowingDeleteForm(instance = showing)
-
-    context = {
-        'form': form,
-        'showing_obj': showing
-    }
-
-    return render(request, "reviews/review_delete.html", context)
-
                                         # ***************
                                         # *** Reviews ***                                        # ***************
                                         # ***************
@@ -267,6 +207,11 @@ def review_delete(request, event_id, review_id):
 
     return render(request, "reviews/review_delete.html", context)
 
+def reviews_view(request, event_id):
+    context = {}
+    context["event"] = Event.objects.filter(event_id=event_id).first()
+
+    return render(request, "event_reviews.html", context)
 
 def review_like(request, review_id):
     review = EventReview.objects.filter(review_id=review_id).first()
